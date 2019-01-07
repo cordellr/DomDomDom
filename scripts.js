7ca0c98f8e326cb0b1c4dd7ecd0c5ca19884378f
cordellr <cordell.rainock@gmail.com>
2019-01-07 17:32:27 -0600

    Hover on squares. Random color when clicked.

--- scripts.js
+++ scripts.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let id = 0;
 
     addSquareBtn.addEventListener("click", function () {
+        //creates square with counted ID
         let square = document.createElement("div");
         square.id = id;
         square.className = "black-square";
@@ -20,17 +21,34 @@ document.addEventListener("DOMContentLoaded", function () {
         document.body.appendChild(square);
         p.style.opacity = "0";
         id++;
+
+        //hover
+        square.addEventListener("mouseenter", function () {
+            p.style.opacity = "1";
+        });
+
+        //no hover
+        square.addEventListener("mouseleave", function () {
+            p.style.opacity = "0";
+        });
+
+        //random color array
+        let colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+            '#E6B333', '#3366E6', '#999966'];
+
+        //function to choose a random color
+        function chooseRandomColor() {
+            let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+            return randomColor;
+        }
+
+        square.addEventListener("click", function () {
+            let randomColor = chooseRandomColor();
+            square.style.backgroundColor = randomColor;
+        })
     })
 
 
-
-    //let colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
-    //'#E6B333', '#3366E6', '#999966'];
-
-    //function chooseRandomColor() {
-    //let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
-    //return randomColor;
-    //}
 
     //h1.addEventListener("dblclick", function () {
     //let randomColor = chooseRandomColor();
